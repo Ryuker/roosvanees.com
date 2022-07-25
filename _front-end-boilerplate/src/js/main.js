e87bdd8c86a756e674b21f26df1b41073e7c2e8d
Joeri van Ees <lifeisagamenotaproduct@gmail.com>
2022-07-25 22:25:41 +0200

    cleaned up slider code

--- _front-end-boilerplate/src/js/main.js
+++ _front-end-boilerplate/src/js/main.js
@@ -98,39 +98,6 @@ jQuery(document).ready(function () {
     slidesToShow: 1,
     slidesToScroll: 1,
     infinite: true,
-    // centerMode: true,
-    // centerPadding: '60px',
-    // responsive: [
-    //   {
-    //     breakpoint: 1400,
-    //     settings: {
-    //       slidesToShow: 4,
-    //       slidesToScroll: 1
-    //     }
-    //   },
-    //   {
-    //     breakpoint: 992,
-    //     settings: {
-    //       slidesToShow: 3,
-    //       slidesToScroll: 1,
-    //     }
-    //   },
-    //   {
-    //     breakpoint: 768,
-    //     settings: {
-    //       slidesToShow: 2,
-    //       slidesToScroll: 1,
-    //     }
-    //   },
-    //   {
-    //     breakpoint: 480,
-    //     settings: {
-    //       autoplay: true,
-    //       slidesToShow: 1,
-    //       slidesToScroll: 1,
-    //     }
-    //   }
-    // ]
   });
 
    
