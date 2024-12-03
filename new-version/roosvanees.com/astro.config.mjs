0716cf0ccd28bc528e918ef0842fcd0f52dbce23
ryuker <lifeisagamenotaproduct@gmail.com>
2024-12-03 20:58:05 +0100

    setup initial project, installed tailwind, installed astro-icon

---
+++ new-version/roosvanees.com/astro.config.mjs
@@ -0,0 +1,20 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import tailwind from "@astrojs/tailwind";
+
+import icon from 'astro-icon';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind(), 
+    icon({
+      iconDir: "src/assets/icons",
+      include: {
+        // mdi: ["*"], // (Default) Loads entire Material Design Icon set
+        // mdi: ["account"], // Loads only Material Design Icon's "account" SVG
+      }
+    })
+  ],
+  output: 'server'
+});
